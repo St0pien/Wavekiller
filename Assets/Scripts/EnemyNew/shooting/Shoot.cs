722493b8b0b0079900fb588fb5f2b1c2a8f1cf12
Rauven <jstepien50@gmail.com>
2019-06-08 20:42:54 +0200

    Add enemy aiming system and delete bad shootAnim

--- Assets/Scripts/EnemyNew/shooting/Shoot.cs
+++ Assets/Scripts/EnemyNew/shooting/Shoot.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public float xOffset; //must be lower than 0
-    public float yOffset; //must be lower than 0
     public float movementSpeed;
     public float timeDelay;
     public GameObject rightHand;
@@ -14,11 +12,10 @@ public class Shoot : MonoBehaviour
     public AudioClip shotSND;
     public AudioSource snd;
     public GameObject pistol;
+    public Quaternion handOffset;
 
     bool shot = false;
     Quaternion startRotation;
-    float maxX;
-    float maxY;
 
     float shotDelay = 0;
     float dist;
@@ -27,8 +24,6 @@ public class Shoot : MonoBehaviour
 	void Start ()
     {
         startRotation = rightHand.transform.localRotation;
-        maxX = rightHand.transform.localRotation.x + xOffset;
-        maxY = rightHand.transform.localRotation.y + yOffset;
     }
 
     // Update is called once per frame
@@ -38,41 +33,17 @@ public class Shoot : MonoBehaviour
         {
             dist = Vector3.Distance(transform.position, GameObject.Find("FPSController").transform.position);
             shotDelay += Time.deltaTime;
-            holdStill();
             decideToShoot();
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                shot = true;
-            }
 
             if (shot)
             {
                 shotDelay = 0;
                 sparks.GetComponent<ParticleSystem>().Play();
                 snd.PlayOneShot(shotSND);
-                shootAnim();
                 checkHit();
             }
         }
 	}
-
-    void shootAnim()
-    {
-        Quaternion rotation = rightHand.transform.localRotation;
-        rotation.x -= xOffset;
-        rotation.y -= yOffset;
-        rightHand.transform.localRotation = Quaternion.Slerp(rightHand.transform.localRotation, rotation, Time.deltaTime*movementSpeed);
-        if (rightHand.transform.localRotation.x < maxX)
-        {
-            shot = false;
-        }
-    }
-
-    void holdStill()
-    {
-
-        rightHand.transform.localRotation = Quaternion.Slerp(rightHand.transform.localRotation, startRotation, Time.deltaTime*movementSpeed*5);
-    }
 
     void decideToShoot()
     {
@@ -105,8 +76,12 @@ public class Shoot : MonoBehaviour
     void setPistolTransform()
     {
         Transform hand = transform.GetChild(2);
-        Vector3 playerPosition = GameObject.FindWithTag("Player").transform.position;
+        Vector3 playerPosition = GameObject.FindWithTag("Player").transform.position+new Vector3(0,1,0);
         hand.position = playerPosition;
-        //hand.rotation = Quaternion.LookRotation(playerPosition - pistol.transform.position);
+        hand.rotation = Quaternion.LookRotation(playerPosition - pistol.transform.position)*handOffset;
+        if(shot)
+        {
+            shot = false;
+        }
     }
 }
