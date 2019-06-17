5f1116ba0e94e5acc6859fbe82bc26ecec2c521a
Rauven <jstepien50@gmail.com>
2019-06-17 20:50:59 +0200

    Make terrain, get ready to border the map

--- Assets/Scripts/GUI/startPosition.cs
+++ Assets/Scripts/GUI/startPosition.cs
@@ -12,7 +12,7 @@ public class startPosition : MonoBehaviour
         float x, z;
         x = Random.Range(200, 800);
         z = Random.Range(200, 800);
-        Vector3 position = new Vector3(x, 25, z);
+        Vector3 position = new Vector3(x, 50, z);
         player.transform.position = position;
 	}
 	
